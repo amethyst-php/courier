f166205e8303bb6a1bd0f989218f85eafbe60915
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/CourierManager.php
+++ src/Managers/CourierManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Courier newEntity()
+ * @method \Amethyst\Schemas\CourierSchema getSchema()
+ * @method \Amethyst\Repositories\CourierRepository getRepository()
+ * @method \Amethyst\Serializers\CourierSerializer getSerializer()
+ * @method \Amethyst\Validators\CourierValidator getValidator()
+ * @method \Amethyst\Authorizers\CourierAuthorizer getAuthorizer()
+ */
 class CourierManager extends Manager
 {
     use ConfigurableManager;
